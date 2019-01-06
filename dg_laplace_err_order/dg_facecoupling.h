e2b3a0dc901737a07187778c082fc0d5c6b7f75c
rrgalvan <rafael.rodriguez@uca.es>
2019-01-06 12:18:36 +0100

    Cleaned up code

--- dg_laplace_err_order/dg_facecoupling.h
+++ dg_laplace_err_order/dg_facecoupling.h
@@ -5,9 +5,11 @@
 
 typedef enum {PLUS, MINUS} CoupledElement;
 
+//----------------------------------------------------------------------
 // Coupling of two FE_Wrapper objets (denoted plus and minus) trough a
 // common face
 class DG_FaceCoupling
+//----------------------------------------------------------------------
 {
  public:
  DG_FaceCoupling(FE_Wrapper const& fe_plus, FE_Wrapper const& fe_minus):
@@ -19,14 +21,6 @@ class DG_FaceCoupling
   // Return one of the two coupled FE_Wrapper objects (PLUS or MINUS)
   template <CoupledElement>
   const FE_Wrapper& fe() const;
-
-  // // Add a bilinear term to an specific Element x Elemenet matrix coupling
-  // template <CoupledElement Elem1, CoupledElement Elem2, class DG_BilinearTerm> void
-  //   add_term(DG_BilinearTerm const& term, Number weight);
-
-  // // Add a bilinear term to internal matrix
-  // template <class DG_Term>
-  //   void integrate_bilinear_term(DG_Term const& term);
 
   unsigned int n_quad_points() const { return _n_quad_points; }
   unsigned int qp() const { return _qp; }
@@ -48,8 +42,8 @@ template <> inline FE_Wrapper const&
 DG_FaceCoupling::fe<MINUS>() const { return _fe_minus; }
 
 //----------------------------------------------------------------------
-
 class DG_Term
+//----------------------------------------------------------------------
 {
 public:
   DG_Term(const DG_FaceCoupling& face_coupling) :
@@ -67,9 +61,6 @@ public:
     DG_Term(face_coupling),
     _penalty(penalty),
     _h_elem(h_elem) {}
-
-  // // Integrate term on a face
-  // void integrate(DG_FaceCoupling*);
 
   // Evalate bilinear form (at current qudrature point) for the
   // unknown basis function id_u and the test basis function id_test
@@ -147,7 +138,6 @@ SIP_BilinearForm::value<MINUS,MINUS>(unsigned int id_u, unsigned int id_test) co
 
 //----------------------------------------------------------------------
 
-
 Number inline
 SIP_BilinearForm::value_plus_plus(unsigned int qp, unsigned int id_u, unsigned int id_test) const
 {
@@ -208,114 +198,15 @@ SIP_BilinearForm::value_minus_minus(unsigned int qp, unsigned int id_u, unsigned
   return 0.5 * (v*(du*normal) + u*(dv*normal)) + (_penalty/_h_elem)*u*v;
 }
 
-// //----------------------------------------------------------------------
-
-// class DG_Term
-// {
-// public:
-//   DG_Term(const DG_FaceCoupling& face_coupling) :
-//     _face(face_coupling) {}
-//   DG_FaceCoupling const& face() const { return _face; };
-// protected:
-//   const DG_FaceCoupling& _face;
-// };
-
-// class SIP_BilinearForm: public DG_Term
-// {
-// public:
-//   SIP_BilinearForm(const DG_FaceCoupling& face_coupling,
-// 		   double penalty, double h_elem):
-//     DG_Term(face_coupling),
-//     _penalty(penalty),
-//     _h_elem(h_elem) {}
-
-
-//   template <CoupledElement Elem1, CoupledElement Elem2>
-//   Number value(unsigned int i, unsigned int j) const;
-// private:
-//   const double _penalty;
-//   const double _h_elem;
-// };
-
-// // void SIP_BilinearForm::integrate(DG_FaceCoupling* face)
-// // {
-// //   for (unsigned int qp=0; qp<_face.n_quad_points(); qp++) // Integrate on face
-// //     {
-// //       const auto weight = _face.fe<PLUS>().JxW[qp];
-// //       face->add_term<PLUS, PLUS, SIP_BilinearForm>(*this, weight);
-// //       face->add_term<PLUS, MINUS,SIP_BilinearForm>(*this, weight);
-// //       face->add_term<MINUS,PLUS, SIP_BilinearForm>(*this, weight);
-// //       face->add_term<MINUS,MINUS,SIP_BilinearForm>(*this, weight);
-// //     }
-// // }
-
-// template <> Number
-// SIP_BilinearForm::value<PLUS,PLUS>(unsigned int unknown_id, unsigned int test_id) const
-// {
-//   // Normal vector, considered in PLUS orientation
-//   auto const& normal = _face.fe<PLUS>().qrule_normals[_face.qp()];
-//   // Unknown
-//   auto const& u  = _face.fe<PLUS>().phi[unknown_id] [_face.qp()];
-//   auto const& du = _face.fe<PLUS>().dphi[unknown_id][_face.qp()]; // Gradient
-//   // Test function
-//   auto const& v  = _face.fe<PLUS>().phi[test_id] [_face.qp()];
-//   auto const& dv = _face.fe<PLUS>().dphi[test_id][_face.qp()]; // Gradient
-
-//   return -0.5 * (v*(du*normal) + u*(dv*normal)) + (_penalty/_h_elem)*u*v;
-// }
-
-// template <> Number
-// SIP_BilinearForm::value<PLUS,MINUS>(unsigned int unknown_id, unsigned int test_id) const
-// {
-//   // Normal vector, considered in PLUS orientation
-//   auto const& normal = _face.fe<PLUS>().qrule_normals[_face.qp()];
-//   // Unknown
-//   auto const& u  = _face.fe<PLUS>().phi[unknown_id] [_face.qp()];
-//   auto const& du = _face.fe<PLUS>().dphi[unknown_id][_face.qp()]; // Gradient
-//   // Test function
-//   auto const& v  = _face.fe<MINUS>().phi[test_id] [_face.qp()];
-//   auto const& dv = _face.fe<MINUS>().dphi[test_id][_face.qp()]; // Gradient
-
-//   return 0.5 * (v*(du*normal) - u*(dv*normal)) - (_penalty/_h_elem)*u*v;
-// }
-
-// template <> Number
-// SIP_BilinearForm::value<MINUS,PLUS>(unsigned int unknown_id, unsigned int test_id) const
-// {
-//   // Normal vector, considered in PLUS orientation
-//   auto const& normal = _face.fe<PLUS>().qrule_normals[_face.qp()];
-//   // Unknown
-//   auto const& u  = _face.fe<MINUS>().phi[unknown_id] [_face.qp()];
-//   auto const& du = _face.fe<MINUS>().dphi[unknown_id][_face.qp()]; // Gradient
-//   // Test function
-//   auto const& v  = _face.fe<PLUS>().phi[test_id] [_face.qp()];
-//   auto const& dv = _face.fe<PLUS>().dphi[test_id][_face.qp()]; // Gradient
-
-//   return 0.5 * (-v*(du*normal) + u*(dv*normal)) - (_penalty/_h_elem)*u*v;
-// }
-
-// template <> Number
-// SIP_BilinearForm::value<MINUS,MINUS>(unsigned int unknown_id, unsigned int test_id) const
-// {
-//   // Normal vector, considered in PLUS orientation
-//   auto const& normal = _face.fe<PLUS>().qrule_normals[_face.qp()];
-//   // Unknown
-//   auto const& u  = _face.fe<MINUS>().phi[unknown_id] [_face.qp()];
-//   auto const& du = _face.fe<MINUS>().dphi[unknown_id][_face.qp()]; // Gradient
-//   // Test function
-//   auto const& v  = _face.fe<MINUS>().phi[test_id] [_face.qp()];
-//   auto const& dv = _face.fe<MINUS>().dphi[test_id][_face.qp()]; // Gradient
-
-//   return 0.5 * (v*(du*normal) + u*(dv*normal)) + (_penalty/_h_elem)*u*v;
-// }
-
 //----------------------------------------------------------------------
-
 template <CoupledElement Elem1, CoupledElement Elem2>
 class face_select {};
+//----------------------------------------------------------------------
 
+//----------------------------------------------------------------------
 template <class Term>
 class FaceIntegrator
+//----------------------------------------------------------------------
 {
 public:
   FaceIntegrator(Term const& t): _face(t.face()), _term(t)
@@ -401,7 +292,6 @@ FaceIntegrator<BilinearForm>::save_to_system(LinearImplicitSystem& system)
   system.matrix->add_matrix(_K_minus_minus, dofs_minus);
 }
 
-// ----------------------------------------------------
 
 template <>
 struct face_select<PLUS,PLUS> {
