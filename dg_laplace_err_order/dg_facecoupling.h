a029d2a8e43ad1ed359ecd1030956495be664adf
J. Rafael Rodríguez Galván <rafael.rodriguez@uca.es>
2019-02-23 14:25:49 +0100

    Fixed bug, orders OK

--- dg_laplace_err_order/dg_facecoupling.h
+++ dg_laplace_err_order/dg_facecoupling.h
@@ -19,9 +19,9 @@ class DG_FaceCoupling
 {
  public:
  DG_FaceCoupling(FE_Wrapper const& fe_plus, FE_Wrapper const& fe_minus):
-   _fe_plus(fe_plus), _fe_minus(fe_minus), _n_quad_points(_fe_plus.n_quad_points())
+   _fe_plus(&fe_plus), _fe_minus(&fe_minus), _n_quad_points(_fe_plus->n_quad_points())
   {
-    assert(_fe_plus.n_quad_points() == _fe_minus.n_quad_points());
+    assert(_fe_plus->n_quad_points() == _fe_minus->n_quad_points());
   }
 
   // Return one of the two coupled FE_Wrapper objects (PLUS or MINUS)
@@ -35,17 +35,17 @@ class DG_FaceCoupling
   void add_to_system(LinearImplicitSystem& sys);
 
  private:
-  const FE_Wrapper& _fe_plus;
-  const FE_Wrapper& _fe_minus;
+  const FE_Wrapper* _fe_plus;
+  const FE_Wrapper* _fe_minus;
   const unsigned int _n_quad_points;
   unsigned int _qp;
 };
 
 template <> inline FE_Wrapper const&
-DG_FaceCoupling::fe<PLUS>() const { return _fe_plus; }
+DG_FaceCoupling::fe<PLUS>() const { return *_fe_plus; }
 
 template <> inline FE_Wrapper const&
-DG_FaceCoupling::fe<MINUS>() const { return _fe_minus; }
+DG_FaceCoupling::fe<MINUS>() const { return *_fe_minus; }
 
 //----------------------------------------------------------------------
 class DG_Term
@@ -104,6 +104,52 @@ Number SIP_BilinearForm::_value(unsigned int qp, unsigned int id_u, unsigned int
   return - ( mean(du*normal)*jump<Elem2>(v) + jump<Elem1>(u)*mean(dv*normal) )
     + (_penalty/_h_elem) * jump<Elem1>(u)*jump<Elem2>(v);
 }
+
+// //----------------------------------------------------------------------
+// class SIP_BilinearForm: public DG_Term
+// //----------------------------------------------------------------------
+// {
+// public:
+//   SIP_BilinearForm(const DG_FaceCoupling& face_coupling,
+// 		   double penalty, double h_elem):
+//     DG_Term(face_coupling),
+//     _penalty(penalty),
+//     _h_elem(h_elem) {}
+
+//   // Evalate bilinear form (at current qudrature point) for the
+//   // unknown basis function id_u and the test basis function id_test
+//   Number inline value_plus_plus(unsigned int qp, unsigned int id_u, unsigned int id_test) const {
+//     return _value<PLUS,PLUS>(qp,id_u,id_test); }
+//   Number inline value_plus_minus(unsigned int qp, unsigned int id_u, unsigned int id_test) const {
+//     return _value<PLUS,MINUS>(qp,id_u,id_test); }
+//   Number inline value_minus_plus(unsigned int qp, unsigned int id_u, unsigned int id_test) const {
+//     return _value<MINUS,PLUS>(qp,id_u,id_test); }
+//   Number inline value_minus_minus(unsigned int qp, unsigned int id_u, unsigned int id_test) const {
+//     return _value<MINUS,MINUS>(qp,id_u,id_test); }
+
+// private:
+//   const double _penalty;
+//   const double _h_elem;
+
+//     template <CoupledElement Elem1, CoupledElement Elem2>
+//   Number _value(unsigned int qp, unsigned int id_u, unsigned int id_test) const;
+// };
+
+// template <CoupledElement Elem1, CoupledElement Elem2> inline
+// Number SIP_BilinearForm::_value(unsigned int qp, unsigned int id_u, unsigned int id_test) const
+// {
+//   // Normal vector, considered in PLUS orientation
+//   auto const& normal = _face.fe<PLUS>().qrule_normals[qp];
+//   // Unknown
+//   auto const& u  = _face.fe<Elem1>().phi[id_u] [qp];
+//   auto const& du = _face.fe<Elem1>().dphi[id_u][qp]; // Gradient
+//   // Test function
+//   auto const& v  = _face.fe<Elem2>().phi[id_test] [qp];
+//   auto const& dv = _face.fe<Elem2>().dphi[id_test][qp]; // Gradient
+
+//   return - ( mean(du*normal)*jump<Elem2>(v) + jump<Elem1>(u)*mean(dv*normal) )
+//     + (_penalty/_h_elem) * jump<Elem1>(u)*jump<Elem2>(v);
+// }
 
 
 //----------------------------------------------------------------------
@@ -188,74 +234,74 @@ InteriorFaceIntegrator<BilinearForm>::save_to_system(LinearImplicitSystem& syste
   system.matrix->add_matrix(_K_minus_minus, dofs_minus);
 }
 
-//----------------------------------------------------------------------
-template <class Term>
-class BoundaryFaceIntegrator
-//----------------------------------------------------------------------
-{
-public:
-  BoundaryFaceIntegrator(Term const& t): _face(t.face()), _term(t)
-  {
-    _Ke.resize   ( _face.fe<PLUS>().n_dofs(), _face.fe<PLUS>().n_dofs() );
-    _Fe.resize   ( _face.fe<PLUS>().n_dofs() )
-  }
+// //----------------------------------------------------------------------
+// template <class Term>
+// class BoundaryFaceIntegrator
+// //----------------------------------------------------------------------
+// {
+// public:
+//   BoundaryFaceIntegrator(Term const& t): _face(t.face()), _term(t)
+//   {
+//     _Ke.resize   ( _face.fe<PLUS>().n_dofs(), _face.fe<PLUS>().n_dofs() );
+//     _Fe.resize   ( _face.fe<PLUS>().n_dofs() );
+//   }
 
-  DenseMatrix<Number>& get_K_plus_plus()   { return _Ke; }
+//   DenseMatrix<Number>& get_K_plus_plus()   { return _Ke; }
 
-  void integrate();
-  void save_to_system(LinearImplicitSystem& system);
+//   void integrate();
+//   void save_to_system(LinearImplicitSystem& system);
 
-private:
-  DenseMatrix<Number> _Ke;
-  DenseVector<Number> _Fe;
-  DG_FaceCoupling const& _face;
-  Term const& _term;
-  unsigned int _qp;
+// private:
+//   DenseMatrix<Number> _Ke;
+//   DenseVector<Number> _Fe;
+//   DG_FaceCoupling const& _face;
+//   Term const& _term;
+//   unsigned int _qp;
 
-  template <CoupledElement Elem1, CoupledElement Elem2> void
-    _integrate_on_qpoint(Number const& weight);
+//   template <CoupledElement Elem1, CoupledElement Elem2> void
+//     _integrate_on_qpoint(Number const& weight);
 
-};
+// };
 
-template <class Term>
-template <CoupledElement Elem1, CoupledElement Elem2> void
-BoundaryFaceIntegrator<Term>::_integrate_on_qpoint(Number const& weight)
-{
-  const unsigned int n_dofs_1 = _face.fe<Elem1>().n_dofs();
-  const unsigned int n_dofs_2 = _face.fe<Elem2>().n_dofs();
-  for (unsigned int i=0; i<n_dofs_1; i++)
-    {
-      for (unsigned int j=0; j<n_dofs_2; j++)
-	{
-	  Number value = _term.value(_qp, i, j);
-	  _Ke(i,j) += weight*value;
-	}
-    }
-}
+// template <class Term>
+// template <CoupledElement Elem1, CoupledElement Elem2> void
+// BoundaryFaceIntegrator<Term>::_integrate_on_qpoint(Number const& weight)
+// {
+//   const unsigned int n_dofs_1 = _face.fe<Elem1>().n_dofs();
+//   const unsigned int n_dofs_2 = _face.fe<Elem2>().n_dofs();
+//   for (unsigned int i=0; i<n_dofs_1; i++)
+//     {
+//       for (unsigned int j=0; j<n_dofs_2; j++)
+// 	{
+// 	  Number value = _term.value(_qp, i, j);
+// 	  _Ke(i,j) += weight*value;
+// 	}
+//     }
+// }
 
-template <class BilinearForm> void
-BoundaryFaceIntegrator<BilinearForm>::integrate()
-{
-  for (_qp=0; _qp<_face.n_quad_points(); _qp++) // Integrate on face
-    {
-      const auto weight = _face.fe<PLUS>().JxW[_qp];
-      _integrate_on_qpoint<PLUS, PLUS> (weight);
-      _integrate_on_qpoint<PLUS, MINUS>(weight);
-      _integrate_on_qpoint<MINUS,PLUS> (weight);
-      _integrate_on_qpoint<MINUS,MINUS>(weight);
-    }
-}
+// template <class BilinearForm> void
+// BoundaryFaceIntegrator<BilinearForm>::integrate()
+// {
+//   for (_qp=0; _qp<_face.n_quad_points(); _qp++) // Integrate on face
+//     {
+//       const auto weight = _face.fe<PLUS>().JxW[_qp];
+//       _integrate_on_qpoint<PLUS, PLUS> (weight);
+//       _integrate_on_qpoint<PLUS, MINUS>(weight);
+//       _integrate_on_qpoint<MINUS,PLUS> (weight);
+//       _integrate_on_qpoint<MINUS,MINUS>(weight);
+//     }
+// }
 
-template <class BilinearForm> void
-BoundaryFaceIntegrator<BilinearForm>::save_to_system(LinearImplicitSystem& system)
-{
-  const auto& dofs_plus  = _face.fe<PLUS>().dof_indices;
-  const auto& dofs_minus = _face.fe<MINUS>().dof_indices;
-  system.matrix->add_matrix(_K_plus_plus, dofs_plus);
-  system.matrix->add_matrix(_K_plus_minus, dofs_plus, dofs_minus);
-  system.matrix->add_matrix(_K_minus_plus, dofs_minus, dofs_plus);
-  system.matrix->add_matrix(_K_minus_minus, dofs_minus);
-}
+// template <class BilinearForm> void
+// BoundaryFaceIntegrator<BilinearForm>::save_to_system(LinearImplicitSystem& system)
+// {
+//   const auto& dofs_plus  = _face.fe<PLUS>().dof_indices;
+//   const auto& dofs_minus = _face.fe<MINUS>().dof_indices;
+//   system.matrix->add_matrix(_K_plus_plus, dofs_plus);
+//   system.matrix->add_matrix(_K_plus_minus, dofs_plus, dofs_minus);
+//   system.matrix->add_matrix(_K_minus_plus, dofs_minus, dofs_plus);
+//   system.matrix->add_matrix(_K_minus_minus, dofs_minus);
+// }
 
 
 //----------------------------------------------------------------------
--- dg_laplace_err_order/dg_laplace_err_order.C
+++ dg_laplace_err_order/dg_laplace_err_order.C
@@ -217,6 +217,14 @@ void assemble_ellipticdg(EquationSystems & es,
               const double h_elem
   		= elem->volume()/elem_side->volume() * 1./pow(elem_b_order, 2.);
 
+	      // // Define a DG face object associated to two elements
+	      // DG_FaceCoupling face(fe_elem_face, fe_neighbor_face);
+	      // // Define SIP bilinear form on that face
+	      // SIP_BilinearForm asip(face, penalty, h_elem);
+	      // // Integrate to local matrices
+	      // InteriorFaceIntegrator<SIP_BilinearForm> asip_integrator(asip);
+	      // asip_integrator.integrate();
+
   	      // Integrate in element face
   	      auto const n_dofs = fe.n_dofs();
   	      auto const n_face_points = fe_elem_face.n_quad_points();
